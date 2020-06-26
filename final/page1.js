062b48a212f91097c8246bb6d2c39b4f19b9138b
kaojia <65601806+kaojia@users.noreply.github.com>
2020-06-27 00:02:42 +0800

    Update page1.js

--- final/page1.js
+++ final/page1.js
@@ -1,3 +1,38 @@
+var RandomInt = (start, end) => {
+
+    let n = end - start + 1
+    // 放大
+    r = Math.random() * n
+    // 無條件捨去
+    r = Math.floor(r)
+    // 位移到start
+    r = r + start
+    return r
+}
+
+//隨機抽取陣列中的元素，遇到相同就重抽
+var getDiffArr = (arr, num) => {
+    let arrOut = [];
+    let ran;
+    for (let i = 0; i < num; i++) {
+        ran = Math.floor(Math.random() * arr.length);
+        // arrOut.push(arr[ran]);  // error
+        arrOut[i] = arr[ran];
+        if (i > 0) {
+            for (var m = 0; m < i; m++) {
+                if (arrOut[m] == arr[ran]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+
+    }
+    return arrOut;
+}
+
+
+
 $(() => {
     // 當文件ready後執行
 
